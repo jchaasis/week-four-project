7f82a178d3df4f64eba5d34c4eb50e910ccc9cfb
jchaasis <jchaasis@gmail.com>
2017-08-16 19:53:08 -0400

    Click to play works.

--- main.js
+++ main.js
@@ -8,37 +8,90 @@
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
+  //on click, add the preview url to the src attribute in the audio tag?
 
 
 //create global variables
 let results = document.querySelector('.results');
+let audio = document.querySelector('audio');
+let searchButton= document.querySelector('#search-button');
+let searchBox = document.querySelector('#search-box');
+// let musicPlayer = document.querySelector('.music-player');
+let preview = '';
 
+//event listener that will commence the fetch
+  searchButton.addEventListener("click", function(){
+    let artist = searchBox.value;
 
-//convert the JSON data
-  function convertData(data){
-    return data.json();
+    //fetch the data from the iTunes api
+      fetch("https://itunes.apple.com/search?term="+artist+"&limit=25")
+
+        .then(convertData)
+        .then(printData);
+  });
+
+  //convert the JSON data
+    function convertData(data){
+      return data.json();
+    }
+
+  //print the data to verify we were able to retrieve the information.
+    function printData(data){
+
+      for( let i = 0; i < data.results.length; i++){
+
+          const song = data.results[i];
+          //create the HTML elements
+          const containerDiv = document.createElement('div');
+          const p = document.createElement('p');
+          const h4 = document.createElement('h4');
+          const img = document.createElement('img');
+
+          //fill the HTML elements with iTunes data
+          img.src = song.artworkUrl100;
+          p.textContent = `${song.trackName}`;
+          h4.textContent = `${song.artistName}`;
+          containerDiv.setAttribute("data_src", `${song.previewUrl}`);
+
+          results.appendChild(containerDiv);
+          containerDiv.appendChild(img);
+          containerDiv.appendChild(p);
+          containerDiv.appendChild(h4);
+
+        };
+          console.log(data);
+
   }
-//print the data to verify we were able to retrieve the information.
-  function printData(data){
-    let searchItems = '';
-    for( let i = 0; i < data.results.length; i++){
-      let searchItem = `
-                      <div>
-                        <img src="${data.results[i].artworkUrl100}">
-                        <h4>${data.results[i].trackName}</h4>
-                        <h3>${data.results[i].artistName}</h3>
-                      </div>
-                      `;
-      searchItems += searchItem;
+  //When a song is clicked, the preview will play through the audio player
 
+  function findContainingDiv(e) {
+    let original = e.target;
+
+    while (original !== null) {
+
+      if (original.tagName === "DIV") {
+        return original;
       }
-      results.innerHTML= searchItems;
-      console.log(data);
-  }
 
-//fetch the data from the itunes API
+      original = original.parentElement;
+    }
 
-fetch("https://itunes.apple.com/search?term=jack+johnson&limit=25")
+    return null;
+    }
 
-  .then(convertData)
-  .then(printData);
+  results.addEventListener("click", function (e) {
+      // when we click anywhere in the container, try
+      // to find the target's containing div.
+      let div = findContainingDiv(e);
+
+      if (div !== null) {
+   // make the preview url the src for the audio tag
+
+      let songPreview = div.getAttribute("data_src");
+
+        audio.src = songPreview;
+
+        // console.log(artistName);
+      }
+
+});
